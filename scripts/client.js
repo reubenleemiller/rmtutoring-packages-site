55f094cec27bfcaecce66b8b03625de82cd86a2c
Reuben Miller <reubenleemiller@gmail.com>
2025-06-05 23:42:26 -0600

    remove test

--- scripts/client.js
+++ scripts/client.js
@@ -1,6 +1,6 @@
 // scripts/client.js
 
-const stripe = Stripe("pk_test_51QyjAMFkTAUuP5b82jYSwMC96AclTfz6Ey02T8nWYXILZrLf1TeWosP42yPAh5tIvcvmadj2bN6T4JwNYiFK1WfS00DmubNBSn"); // Replace with your real publishable key
+const stripe = Stripe("pk_live_51QyjAMFkTAUuP5b8POjVyVCKi0ry2R54UQz4nZaTyWJYSSYPdXliMTvkS256IoT0iSL323qcR90mZjfbH3PU8Wed00Bs0TS9MZ"); // Replace with your real publishable key
 
 let elements;
 
